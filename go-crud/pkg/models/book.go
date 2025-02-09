a075169d92410152d8b9d30f8789260d85023041
paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-02-09 13:24:51 +0530

    crud completed

--- go-crud/pkg/models/book.go
+++ go-crud/pkg/models/book.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"go-crud/pkg/config"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"column:name" json:"name"`
+	Author      string `gorm:"column:author" json:"author"`
+	Publication string `gorm:"column:publication" json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
