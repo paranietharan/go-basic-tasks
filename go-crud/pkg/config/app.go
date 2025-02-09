c00ec9dbdc08287f797b9da498be4ed6d0cb2542
paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-02-09 11:52:45 +0530

    basic

--- go-crud/pkg/config/app.go
+++ go-crud/pkg/config/app.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "root:root@tcp(localhost:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"
+
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to MySQL:", err)
+	}
+
+	db = d
+	log.Println("Connected to MySQL successfuly....")
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
